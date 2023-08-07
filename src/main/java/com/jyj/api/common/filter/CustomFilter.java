e412e73bd83f0e73922cd604ad1504b76d843c28
주윤재 <yoonjae.ju@fssuniverse.com>
2023-08-07 17:52:35 +0900

    로깅 메시지 수정

--- src/main/java/com/jyj/api/common/filter/CustomFilter.java
+++ src/main/java/com/jyj/api/common/filter/CustomFilter.java
@@ -2,6 +2,7 @@ package com.jyj.api.common.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
@@ -24,40 +25,40 @@ public class CustomFilter implements Filter {
         ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
         ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
 
+        String threadId = Long.toString(Thread.currentThread().getId());
+        MDC.put("threadId", threadId);
+
         try {
             chain.doFilter(wrappedRequest, wrappedResponse);
 
             logMessage(wrappedRequest, wrappedResponse);
 
         } finally {
-            // 필터 체인 수행 후 response의 내용을 복원하도록 합니다.
             wrappedResponse.copyBodyToResponse();
+            MDC.remove("threadId");
         }
     }
 
     private static void logMessage(ContentCachingRequestWrapper wrappedRequest, ContentCachingResponseWrapper wrappedResponse) {
-        // 필터에서 로깅할 메시지 생성
+
         String threadId = UUID.randomUUID().toString();
         StringBuilder logMessage = new StringBuilder();
         logMessage.append("HTTP ACCESS LOG FILTER:\n");
-        logMessage.append("threadId: ").append(UUID.randomUUID().toString()).append("\n");
+        logMessage.append("threadId: ").append(MDC.get("threadId")).append("\n");
         logMessage.append("method: ").append(wrappedRequest.getMethod()).append("\n");
         logMessage.append("url: ").append(wrappedRequest.getRequestURI()).append("\n");
         logMessage.append("userAgent: ").append(wrappedRequest.getHeader("User-Agent")).append("\n");
         logMessage.append("host: ").append(wrappedRequest.getHeader("host")).append("\n");
         logMessage.append("clientIp: ").append(wrappedRequest.getRemoteAddr()).append("\n");
 
-        //요청 본문 로깅
         byte[] requestBodyBytes = wrappedRequest.getContentAsByteArray();
         String requestBody = new String(requestBodyBytes, StandardCharsets.UTF_8);
         logMessage.append("requestParams: ").append(requestBody).append("\n");
 
-        // 응답 본문 로깅
         byte[] responseBodyBytes = wrappedResponse.getContentAsByteArray();
         String responseBody = new String(responseBodyBytes, StandardCharsets.UTF_8);
         logMessage.append("responseParams: ").append(responseBody).append("\n");
 
-        // 로깅 (INFO 레벨로 로그 출력)
         log.info(logMessage.toString());
     }
 
